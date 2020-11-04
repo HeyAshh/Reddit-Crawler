c31d0750852229b344fc2c33b1a37fe65d4cc3da
Ashh <heyashh1@gmail.com>
2020-11-05 00:40:26 +0300

    third terminal commit

---
+++ sheet.js
@@ -0,0 +1,24 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+module.exports = class Sheet {
+    constructor() {
+        this.doc = new GoogleSpreadsheet('');
+    }
+    async load() {
+        await this.doc.useServiceAccountAuth(require('./credentials.json'));
+        await this.doc.loadInfo();
+    }
+    async addSheet(name){
+        const newSheet = await this.doc.addSheet({ title });
+    }
+
+    async addRows(rows, i) { 
+        const sheet = this.doc.sheetsByIndex[i];
+        await sheet.addRows(rows);
+    }
+    async getRows(i){
+        const sheet = this.doc.sheetsByIndex[i];
+        const rows = await sheet.getRows();
+        return rows;
+    }
+}
