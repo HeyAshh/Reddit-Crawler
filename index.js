978c2281362d3375a054bcb1375ad0bfb1158a1e
Ashh <Heyashh1@gmail.com>
2020-11-06 00:51:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const url = 'https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_
         const pointsB = Number(b.points.split(' ')[0])
         return pointsB - pointsA;
     })
-     console.log({formattedComments});
+     console.log(formattedComments.slice(0, 10));
     //sort comments by points
 
 
